a45d04d11ece5a164e0ab00c68933bc631401a60
pasupathivadivel <43200473+pasupathivadivel@users.noreply.github.com>
2018-09-25 16:48:35 +0530

    Update honeywell.js

--- www/honeywell.js
+++ www/honeywell.js
@@ -2,6 +2,7 @@ var exec = require('cordova/exec');
 
 var honeywell = {
     listenForScans: function(success, failure) {
+        alert("ssasssssssssssss");
         return exec(success, failure, "HoneywellScannerPlugin", "listenForScans", []);
     }
 };
